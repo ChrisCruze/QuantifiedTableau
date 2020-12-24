093c2d7fffae20e047bdfeaf43b1503230d9b6c9
ChrisCruze <cruzc09@gmail.com>
2020-12-24 00:55:16 -0500

    added todoist-connector

--- src/Components/Molecules/TableauConnectorButton.js
+++ src/Components/Molecules/TableauConnectorButton.js
@@ -2,16 +2,22 @@ import React, { Component, Fragment, useEffect } from "react";
 
 const TableauConnectorButton = ({ func }) => {
   return (
-    <a
-      class="navbar-minimalize minimalize-styl-2 btn btn-primary "
-      href="#"
-      onClick={e => {
-        e.preventDefault();
-        func();
-      }}
-    >
-      Pull Data
-    </a>
+    <Fragment>
+      <button type="button" id="submitButton" className="btn btn-success" style={{ margin: "10px" }}>
+        Get Earthquake Data!
+      </button>
+
+      <a
+        class="navbar-minimalize minimalize-styl-2 btn btn-primary "
+        href="#"
+        onClick={e => {
+          e.preventDefault();
+          func();
+        }}
+      >
+        Pull Data
+      </a>
+    </Fragment>
   );
 };
 export default TableauConnectorButton;
